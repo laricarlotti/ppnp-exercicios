42b751c1dbcee92c80b42ffdb53bf2e8cd73be07
nataliavalentin <nataliaferreiravalentin@gmail.com>
2020-02-17 17:10:41 -0300

    Finish exercise 2

--- aula1.py
+++ aula1.py
@@ -13,3 +13,27 @@ valorDolar = valorReal * taxaCambio
 print(valorDolar)
 
 
+# ex2 | saque no caixa eletrônico
+
+# Crie um programa que simule um saque feito no caixa
+# eletrônico. O programa deve solicitar ao usuário o número
+# da conta, sua senha e valor para saque.
+# Com essas informações, ele deve verificar se o número da
+# conta e a senha são válidos, e se o saldo é suficiente para
+# o saque.
+# Por fim, ele deve mostrar o saldo atualizado na tela.
+
+numeroConta = int(input("Qual é o número da sua conta? "))
+senhaConta = input("Digite a sua senha ")
+valorSaque = int(input("Quanto você quer sacar? "))
+
+numeroBanco = 12345
+senhaBanco = "abcde"
+saldoBanco = 15000
+
+saldoFinal = saldoBanco - valorSaque
+
+if numeroConta == numeroBanco and senhaConta == senhaBanco:
+  print(saldoFinal)
+else:
+  print("Transação inválida")
