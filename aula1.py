d6847a002f87dccae9d33eed22a04d1bee884ad3
LariCarlotti <larissa.carlotti@gmail.com>
2020-02-17 16:47:11 -0300

    Starting first exercise

---
+++ aula1.py
@@ -0,0 +1,12 @@
+# ex1 | Conversão de moedas
+
+# Crie um programa que faça a conversão de um valor em
+# reais para dólares americanos.
+# O programa deve solicitar o valor em reais para o usuário
+# e, usando uma taxa de câmbio fixa, deve mostrar na tela
+# o resultado da conversão. 
+
+valorReal = int(input("Digite o valor em reais"))
+taxaCambio = 4.18
+
+valorDolar = 
